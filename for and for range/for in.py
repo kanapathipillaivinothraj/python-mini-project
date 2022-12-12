7ecd1493e0289a50b1730d6c5237bfd9066a16f7
Kanapathipillaivinothraj <Kanapathipillaivinothraj@gmail.com>
2022-12-12 23:52:19 +0530

    fix: update for-in and for-range loops

--- for and for range/for in.py
+++ for and for range/for in.py
@@ -1,8 +1,23 @@
-A = ["Vinothraj","Mathu","Dilakshan",1,2,3,4,5,6,7,8,89,99,100]
-
+A = ["Vinothraj", "Mathushan", "Dilakshan",
+     1, 2, 3, 4, 5, 6, 7, 8, 89, 99, 100]
 
 
 # for in Loop
 
 for item in A:
     print(item)
+print("finish")
+A = ["A", "B", "C"]
+
+
+for name in A:
+    print(name + " this is for in loop")
+print("finish")
+
+
+Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for num in Number:
+    print("*" * num)
+print("finish")
