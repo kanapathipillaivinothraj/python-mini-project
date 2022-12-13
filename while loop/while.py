318cab0f9b3b78fa6b4f4e44d5c93821db008ed5
Kanapathipillaivinothraj <Kanapathipillaivinothraj@gmail.com>
2022-12-14 00:41:50 +0530

    fix: while loop and while not loop

--- while loop/while.py
+++ while loop/while.py
@@ -11,7 +11,7 @@ output = {
 # while loop and while not loop
 
 
-Dateofbirth = input("Enter Your date fo birth : ").split("/" or ".")
+Dateofbirth = input("Enter Your date fo birth : ")
 
 
 """
@@ -20,8 +20,14 @@ print(f" your value is: {Dateofbirth}")
 print(output["DOB"])
 
 """
-while Dateofbirth == "" :
-    print("plz enter your value: ")
-    Dateofbirth = input("Enter Your date fo birth : ").split("/" or ".")
+while Dateofbirth == "":
+    print("plz enter your Your date fo birth ")
+    Dateofbirth = input("Enter Your date fo birth : ")
 
 
+# split("/" or ".")
+while  Dateofbirth != "":
+    output["DOB"] = str(Dateofbirth)
+    print(f"your Your date fo birth is: {Dateofbirth}")
+    print(output["DOB"])
+    Dateofbirth = input("Enter Your date fo birth : ")
